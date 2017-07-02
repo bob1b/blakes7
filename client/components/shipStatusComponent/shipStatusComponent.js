151080aac9fd52a4a92bde1e3f0c41e91ea9daba
bob1b <bob1b@users.noreply.github.com>
2017-07-02 16:26:40 -0700

    add adapters for shipStatusComponent:force wall, flare shield, neutron blasters cleared. Add speed settings view and cancel

--- client/components/shipStatusComponent/shipStatusComponent.js
+++ client/components/shipStatusComponent/shipStatusComponent.js
@@ -23,61 +23,89 @@ function ShipStatusComponent( options ){
   this.model = {
     speed:0,
     loc:{x:0, y:0},
-    speedSelectVisible:false,
     forceWallActive:true,
     radiationFlareShieldActive:true,
-    neutronBlastersCleared:true
+    neutronBlastersCleared:true,
+    showSettings:false
   };
+
+  function _format_speed(model, type){
+	  var _spd = 'stopped';
+      var speed_num  = 0;
+	  var proto = model.proto;
+
+      // convert standard-by for non-liberator and non-scorpio class ships
+      if (type === "current_speed"){
+		  if (typeof(model.speed) !== "undefined" && model.speed !== 'unknown'){
+            speed_num = model.speed;
+          } else {
+            speed_num = 0;
+          }
+      } else { // type = max speed
+          if (typeof(model.proto) !== "undefined" &&
+              typeof(model.proto.navigation) !== "undefined" && 
+              typeof(model.proto.navigation.maxSpeed) !== "undefined" && 
+              model.proto.navigation.maxSpeed !== "unknown"){
+            speed_num = model.proto.navigation.maxSpeed;
+          } else {
+            speed_num = 0;
+          }
+      }
+
+      // TODO - perhaps convert ship types to ids instead of names
+	  if (typeof(proto) !== 'undefined' && proto.shipType !== 'liberator' && proto.shipType !== 'scorpio'){
+		  _spd = "<span title='time distort'>TD</span>" + SBtoTD(speed_num);
+      } else {
+		  _spd = "<span title='standard by'>SB</span>" + speed_num;
+      }
+
+	  return _spd;
+  }
 
   // define how the model is displayed to the user
   this.views = [
       {
         // returns html to be placed in destination element
         renderer: function(model){
+          var html;
 
           if (model == undefined || $.isEmptyObject(model)){
             return;
           }
 
-          var _spd = 'stopped';
-          var proto = model.proto;
-
-          if (model.speed && model.speed !== 'unknown'){
-            // convert standard-by for non-liberator and non-scorpio class ships
-
-            // TODO - perhaps convert ship types to ids instead of names
-            if (proto.shipType !== 'liberator' && proto.shipType !== 'scorpio'){
-              _spd = "<span title='time distort'>TD</span>" + SBtoTD(model.speed);
-            } else {
-              _spd = "<span title='standard by'>SB</span>" + model.speed;
-            }
-          }
-
+          var _spd = _format_speed(model, "current_speed");
+          var _max_speed = _format_speed(model, "max_speed");
           var _dmg = "none"; // TODO
           var _loc = round2(model.loc.x) + " x " + round2(model.loc.y);
 
-          var speedMenuHTML = "<div class='speedSelector {0}'>asdfasdf</div>"
-            .format( (model.speedSelectVisible ? 'visible' : '') );
 
+          var mainTempl = String("<ul class='info'>" +
+                                 "<li><b>Location:</b> {0}</li>" +
+                                 "<li><b>Speed:</b>" +
+                                   "<a class='showSpeedSelector' href='javascript:;'>{1}</a>" +
+                                 "</li>" +
+                                 "<li><b>Force wall:</b> <a class='toggleFW' href='javascript:;'>{2}</a></li>" +
+                                 "<li><b>Radiation flare shield:</b> <a class='toggleFS' href='javascript:;'>{3}</a></li>" +
+                                 "<li><b>Neutron blasters cleared:</b> <a class='toggleNB' href='javascript:;'>{4}</a></li>" +
+								 "<li><b>Damaged systems:</b> {5}</li>"); // TODO joined list of general systems
 
-          var templ = String("<ul class='info'>" +
-                                "<li><b>Location:</b> {0}</li>" +
-                                "<li><b>Speed:</b>" +
-                                  "<a class='showSpeedSelector' href='javascript:;'>{1}</a>" +
-                                  "{2}" +
-                                "</li>" +
-                                "<li><b>Force wall:</b> <a href='javascript:;'>{3}</a></li>" +
-                                "<li><b>Radiation flare shield:</b> <a href='javascript:;'>{4}</a></li>" +
-                                "<li><b>Neutron blasters cleared:</b> <a href='javascript:;'>{5}</a></li>" +
-                                "<li><b>Damaged systems:</b> {6}</li>"); // TODO joined list of general systems
+           var speedTempl = String("<div class='settingsFrame'>" +
+                                   "<ul><li><b>Current speed:</b> {0}</li>" +
+                                       "<li><b>Maximum speed:</b> {1}</li>" +
+                                       "<li><b>New speed:</b><input type='text' class='new_speed' /></li>" +
+                                       "<li><a class='cancel' href='javascript:;'>Cancel</a></li>");
 
-          var html = String(templ).format(_loc, // calced above
-                                          _spd, // calculated above
-                                          speedMenuHTML,
-                                          model.forceWallActive,
-                                          model.radiationFlareShieldActive,
-                                          model.neutronBlastersCleared,
-                                          _dmg); // calced above
+          if ( !model.showSettings ){
+              html = String(mainTempl).format(_loc,
+                                              _spd,
+                                              model.forceWallActive,
+                                              model.radiationFlareShieldActive,
+                                              model.neutronBlastersCleared,
+                                              _dmg);
+          } else if ( model.showSettings === "speed" ){
+console.log(model);
+              html = String(speedTempl).format(_spd, _max_speed);
+          }
  
           return html;
         },
@@ -99,15 +127,41 @@ function ShipStatusComponent( options ){
       type:'jquery-click',
       selector:'.shipStatusComponent a.showSpeedSelector',
       callback:function(args){ // args: model (a copy), setComponentState
-          var speedSelectVisible = args.model.speedSelectVisible;
-          if (typeof(speedSelectVisible) === "undefined" || !speedSelectVisible){
-            args.model.speedSelectVisible = true;
-          } else {
-            args.model.speedSelectVisible = false;
-          }
-		  console.log('new speedSelectVisible state = ' + args.model.speedSelectVisible);
+          args.model.showSettings = "speed";
           args.setComponentState(args.model);
-       }
+      }
+    },
+    {
+      type:'jquery-click',
+      selector:'.shipStatusComponent a.cancel',
+      callback:function(args){
+          args.model.showSettings = false;
+          args.setComponentState(args.model);
+      }
+    },
+    {
+      type:'jquery-click',
+      selector:'.shipStatusComponent a.toggleFW',
+      callback:function(args){
+          args.model.forceWallActive = !args.model.forceWallActive;
+          args.setComponentState(args.model);
+      }
+    },
+    {
+      type:'jquery-click',
+      selector:'.shipStatusComponent a.toggleFS',
+      callback:function(args){
+          args.model.radiationFlareShieldActive = !args.model.radiationFlareShieldActive;
+          args.setComponentState(args.model);
+      }
+    },
+    {
+      type:'jquery-click',
+      selector:'.shipStatusComponent a.toggleNB',
+      callback:function(args){
+          args.model.neutronBlastersCleared = !args.model.neutronBlastersCleared;
+          args.setComponentState(args.model);
+      }
     }
   ];
 
